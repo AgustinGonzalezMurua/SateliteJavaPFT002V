dd94b2452c2abee849e7a0f86e30c51ea78e1a15
Emmanuel Padilla <jpad.ortega@gmail.com>
2016-10-11 22:04:55 -0300

    Inicialización de satelite java
    TKM :3

---
+++ Satelite/build/generated-sources/jax-ws/org/tempuri/ValidarUsuario.java
@@ -0,0 +1,93 @@
+
+package org.tempuri;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="usuario" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="contrasena" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "usuario",
+    "contrasena"
+})
+@XmlRootElement(name = "ValidarUsuario")
+public class ValidarUsuario {
+
+    @XmlElementRef(name = "usuario", namespace = "http://tempuri.org/", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> usuario;
+    @XmlElementRef(name = "contrasena", namespace = "http://tempuri.org/", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> contrasena;
+
+    /**
+     * Obtiene el valor de la propiedad usuario.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * Define el valor de la propiedad usuario.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setUsuario(JAXBElement<String> value) {
+        this.usuario = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad contrasena.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * Define el valor de la propiedad contrasena.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setContrasena(JAXBElement<String> value) {
+        this.contrasena = value;
+    }
+
+}
