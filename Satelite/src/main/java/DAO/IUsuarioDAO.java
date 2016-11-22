f3b3d43d9e347a9dce97417337e33526ac6f892f
kathyTorres <katherine.93824736@gmail.com>
2016-11-22 00:43:18 -0300

    ELIMINARLIMPIARDATOS
    . Se corrige error al listar en tabla
    - se soluciona duplicado al buscar en tabla
    - se agrega mensaje de error en caso de no existir la busqueda,
    - se comienza a implementar el agregar usuario
    - se habilita limpiar contenido.

--- Satelite/src/main/java/DAO/IUsuarioDAO.java
+++ Satelite/src/main/java/DAO/IUsuarioDAO.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
  */
 public interface IUsuarioDAO extends IBaseDAO{
     public DTO.Usuario Autenticar(String usuario, String contrasena);
-    public DTO.Usuario CrearUsuario(String nombre, String run, int telefono, String email, int tipo_usuario, 
-                                    String contrasena );
+    /*public DTO.Usuario CrearUsuario(String nombre, String run, int telefono, String email, int tipo_usuario, 
+                                    String contrasena ); */
     public DTO.Usuario ObtenerUsuario(String RUN);
     public ArrayList<DTO.Usuario> RecuperarUsuario_Todos();
 
